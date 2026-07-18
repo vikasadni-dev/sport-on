6b0543b1c8a08896d825300dfb1b69e863702c2e
Novika Sari Dwi Cahyani <vikasadni@gmail.com>
2026-07-18 18:07:54 +0700

    header, (50% hero)

---
+++ app/(landing)/components/ui/button.tsx
@@ -0,0 +1,33 @@
+type TButtonProps = {
+  children: React.ReactNode;
+  className?: string;
+  variant?: "primary-blue" | "navy-dark" | "ghost";
+  size?: "normal" | "small";
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = ({children, className, variant = "primary-blue", size = "normal", ...props}: TButtonProps) => {
+
+  const baseStyles =
+    "inline-flex gap-2 duration-300 justify-center items-center cursor-pointer hover:scale-105";
+
+  const variants = {
+    "primary-blue": "bg-primary-blue text-white hover:bg-primary-blue/85",
+    "navy-dark": "bg-navy-dark text-white hover:bg-navy-dark/85",
+    "ghost": "bg-transparent hover:bg-gray-100 text-navy-dark",
+  };
+
+  const sizes = {
+    normal: "py-4 px-9",
+    small: "py-[10px] px-7",
+  };
+
+  return (
+    <button
+      className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`} {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+import HeroSection from "./components/home/hero";
+
+export default function Home() {
+  return (
+    <main>
+      <HeroSection />
+      </main>
+  )
+}
