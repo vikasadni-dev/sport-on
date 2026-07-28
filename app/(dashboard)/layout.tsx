ef2702856f811e7b0261a59a27cdfe1ebdf96050
Novika Sari Dwi Cahyani <vikasadni@gmail.com>
2026-07-28 23:44:07 +0700

    live session 4 part III

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${poppins.variable} antialiased`}>
         <div className="flex min-h-screen bg-white">
-          <Sidebar />
+          <Sidebar/>
           <main className="flex-1 ml-80 p-14 bg-[#F7F9FA] min-h-screen">
             <div className="max-w-6xl mx-auto">{children}</div>
           </main>
